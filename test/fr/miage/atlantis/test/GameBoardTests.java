23f21647bf2eeecbb2ca400c72b9a3853ee4ec0d
Guillaume Lesniak <me@xplod.fr>
2014-02-28 23:22:52 +0100

    tests: Early skeleton for tests

---
+++ test/fr/miage/atlantis/test/GameBoardTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014 Loris Durand, Guillaume Lesniak, Cristian Sanna,
+ *                    Lucie Wiemert
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.miage.atlantis.test;
+
+import fr.miage.atlantis.board.BeachTile;
+import fr.miage.atlantis.board.GameBoard;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Guigui
+ */
+public class GameBoardTests extends TestCase {
+    
+    private GameBoard mGameBoard;
+    
+    public GameBoardTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        mGameBoard = new GameBoard();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testWaterEdge() {
+        // On créé un board avec une seule tile pour le moment
+    }
+    
+}
