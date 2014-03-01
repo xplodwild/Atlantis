d6eb8b4f3dea65d5d68f1c43240418787a79d820
Guillaume Lesniak <me@xplod.fr>
2014-03-01 01:05:04 +0100

    GameBoardTests: Add Game Tile tests

--- test/fr/miage/atlantis/test/GameBoardTests.java
+++ test/fr/miage/atlantis/test/GameBoardTests.java
@@ -18,7 +18,15 @@
 package fr.miage.atlantis.test;
 
 import fr.miage.atlantis.board.BeachTile;
+import fr.miage.atlantis.board.ForestTile;
 import fr.miage.atlantis.board.GameBoard;
+import fr.miage.atlantis.board.GameTile;
+import fr.miage.atlantis.board.MountainTile;
+import fr.miage.atlantis.board.WaterTile;
+import fr.miage.atlantis.entities.Boat;
+import fr.miage.atlantis.entities.SeaSerpent;
+import fr.miage.atlantis.entities.Shark;
+import fr.miage.atlantis.entities.Whale;
 import static junit.framework.Assert.assertTrue;
 import junit.framework.TestCase;
 import org.junit.After;
@@ -94,6 +102,77 @@ public class GameBoardTests extends TestCase {
         
         // On peut replacer une tile à l'endroit cité
         assertTrue(mGameBoard.canPlaceTileAt(5, 5));
+    }
+    
+    @Test
+    public void testGameTile() {
+        GameTile tiles[] = new GameTile[5];
+        
+        // Test de position
+        tiles[0] = new BeachTile(mGameBoard, 1, 1);
+        tiles[1] = new ForestTile(mGameBoard, 1, 2);
+        tiles[2] = new MountainTile(mGameBoard, 1, 3);
+        tiles[3] = new WaterTile(mGameBoard, 1, 4);
+        tiles[4] = new WaterTile(mGameBoard, 1, 5);
+        
+        for (int i = 0; i < 4; i++) {
+            GameTile tile = tiles[i];
+            assertEquals(tile.getX(), 1);
+            assertEquals(tile.getY(), i+1);
+        }
+        
+        // Test ajout d'entités sur une tile d'eau
+        Boat b = new Boat(tiles[3]);
+        Shark s = new Shark(tiles[3]);
+        Whale w = new Whale(tiles[3]);
+        SeaSerpent k = new SeaSerpent(tiles[3]);
+        tiles[3].addEntity(b);
+        
+        assertEquals(tiles[3].getEntities().size(), 1);
+        
+        // On ne peut pas ajouter une entité à une autre tile sans la détacher
+        try {
+            tiles[4].addEntity(b);
+            assertTrue(false);
+        } catch (IllegalStateException e) {
+            // Cette exception DOIT se déclencher
+            assertTrue(true);
+        }
+        
+        tiles[3].removeEntity(b);
+        
+        // Par contre, on ne peut pas ajouter un bateau/requin/baleine/kraken
+        // sur de la terre!
+        try {
+            tiles[0].addEntity(b);
+            assertTrue(false);
+        } catch (IllegalStateException e) {
+            // Cette exception DOIT se déclencher
+            assertTrue(true);
+        }
+        try {
+            tiles[0].addEntity(s);
+            assertTrue(false);
+        } catch (IllegalStateException e) {
+            // Cette exception DOIT se déclencher
+            assertTrue(true);
+        }
+        try {
+            tiles[0].addEntity(w);
+            assertTrue(false);
+        } catch (IllegalStateException e) {
+            // Cette exception DOIT se déclencher
+            assertTrue(true);
+        }
+        try {
+            tiles[0].addEntity(k);
+            assertTrue(false);
+        } catch (IllegalStateException e) {
+            // Cette exception DOIT se déclencher
+            assertTrue(true);
+        }
+        
+        
     }
     
 }
