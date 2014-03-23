8054ba11e45c4cedffb9a6acb5e96ac6fb0e65f9
Cristian SANNA <krysx3@live.fr>
2014-03-23 02:21:31 +0100

    GuiStartScreen : started implementation

---
+++ src/fr/miage/atlantis/gui/controllers/StartScreenController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.atlantis.gui.controllers;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+
+public class StartScreenController implements ScreenController {
+            
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+    }
+    
+    @Override
+    public void onStartScreen() {
+    }
+    
+    @Override
+    public void onEndScreen() {
+        
+    }
+}     
