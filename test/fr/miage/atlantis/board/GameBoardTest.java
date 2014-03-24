8a552caf59a0e05c2bf8349bc74a6e5b26e5ac63
Loris <tsubaki57@gmail.com>
2014-03-24 15:42:49 +0100

    nothing

--- test/fr/miage/atlantis/board/GameBoardTest.java
+++ test/fr/miage/atlantis/board/GameBoardTest.java
@@ -135,7 +135,7 @@ public class GameBoardTest {
         int b = 0;
         int f = 0;
         int m = 0;
-        for(int i=0;i<40;i++){
+        for(int i=0;i<3;i++){
             GameTile result = instance.generateRandomTile();
             if(result instanceof BeachTile) b++;
             if(result instanceof ForestTile) f++;
