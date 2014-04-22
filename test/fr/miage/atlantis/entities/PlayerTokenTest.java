847bb185280bde19577ad341f38e8534e285d365
Lulu <lucie.wiemert@gmail.com>
2014-04-22 19:26:24 +0200

    PlayerTokenTest : add test testMoveToTile() + clean up

--- test/fr/miage/atlantis/entities/PlayerTokenTest.java
+++ test/fr/miage/atlantis/entities/PlayerTokenTest.java
@@ -5,7 +5,9 @@
 package fr.miage.atlantis.entities;
 
 import fr.miage.atlantis.Player;
+import fr.miage.atlantis.board.GameBoard;
 import fr.miage.atlantis.board.GameTile;
+import fr.miage.atlantis.board.NullGameLogic;
 import fr.miage.atlantis.logic.GameLogic;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -45,95 +47,27 @@ public class PlayerTokenTest {
     @Test
     public void testMoveToTile() {
         System.out.println("moveToTile");
-        GameLogic logic = null;
-        GameTile tile = null;
-        PlayerToken instance = null;
-        boolean expResult = false;
-        boolean result = instance.moveToTile(logic, tile);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        GameLogic logic = new NullGameLogic();
+        GameBoard board = new GameBoard();
+        
+        GameTile tile = board.getTileSet().get("Beach #15");
+        GameTile tile1 = board.getTileSet().get("Water #37");
+        Player luigi = new Player("Luigi", 1);
+        PlayerToken pt1 = new PlayerToken(luigi, 5);
+        
+        //1er test on met sur le pion sur une tile
+        pt1.moveToTile(logic, tile);
+        assertTrue(tile.getEntities().contains(pt1));
+        
+        //2ème test : on déplace le pion sur une autre tile
+        pt1.moveToTile(logic, tile1);
+        assertTrue(tile1.getEntities().contains(pt1));
+        
+        //3ème test : on vérifie que le pion n'est plus sur la première tile
+        assertFalse(tile.getEntities().contains(pt1));
+        
+        
+
     }
 
-    /**
-     * Test of getState method, of class PlayerToken.
-     */
-    @Test
-    public void testGetState() {
-        System.out.println("getState");
-        PlayerToken instance = null;
-        int expResult = 0;
-        int result = instance.getState();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPoints method, of class PlayerToken.
-     */
-    @Test
-    public void testGetPoints() {
-        System.out.println("getPoints");
-        PlayerToken instance = null;
-        int expResult = 0;
-        int result = instance.getPoints();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPlayer method, of class PlayerToken.
-     */
-    @Test
-    public void testGetPlayer() {
-        System.out.println("getPlayer");
-        PlayerToken instance = null;
-        Player expResult = null;
-        Player result = instance.getPlayer();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getBoat method, of class PlayerToken.
-     */
-    @Test
-    public void testGetBoat() {
-        System.out.println("getBoat");
-        PlayerToken instance = null;
-        Boat expResult = null;
-        Boat result = instance.getBoat();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setState method, of class PlayerToken.
-     */
-    @Test
-    public void testSetState() {
-        System.out.println("setState");
-        int state = 0;
-        PlayerToken instance = null;
-        instance.setState(state);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setBoat method, of class PlayerToken.
-     */
-    @Test
-    public void testSetBoat() {
-        System.out.println("setBoat");
-        Boat b = null;
-        PlayerToken instance = null;
-        instance.setBoat(b);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 }
