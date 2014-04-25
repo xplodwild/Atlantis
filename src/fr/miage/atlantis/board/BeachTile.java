3d0343d0ebe37fd05905b695d51ca17a3efc4f9e
Lulue <lucie.wiemert@gmail.com>
2014-04-25 14:51:48 +0200

    BeachTile : add comment javadoc

--- src/fr/miage/atlantis/board/BeachTile.java
+++ src/fr/miage/atlantis/board/BeachTile.java
@@ -56,7 +56,14 @@ public class BeachTile extends GameTile {
     public BeachTile(GameBoard board, GameTile hg, GameTile hd, GameTile g, GameTile d, GameTile bd, GameTile bg, String name, int height) {
         super(board, hg, hd, g, d, bd, bg, name, 1);
     }
-    
+
+    /**
+     * Constructeur de BeachTile
+     * 
+     * @param board Plateau auquel appartient la tile
+     * @param stream chargement fichier
+     * @throws IOException 
+     */
     public BeachTile(GameBoard board, DataInputStream stream) throws IOException {
         super(board, stream);
         readSerialized(stream);
