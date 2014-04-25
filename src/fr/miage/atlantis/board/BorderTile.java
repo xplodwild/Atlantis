9ab17a77199fd43bdf55583925aa9fb37e82e4b4
Lulue <lucie.wiemert@gmail.com>
2014-04-25 15:06:57 +0200

    BorderTile add comments javadoc

--- src/fr/miage/atlantis/board/BorderTile.java
+++ src/fr/miage/atlantis/board/BorderTile.java
@@ -31,7 +31,7 @@ import java.io.IOException;
 public class BorderTile extends GameTile {
 
     /**
-     * Attribut discriminant si oui ou non le tile frontiere est
+     * Attribut discriminant si oui ou non le tile frontiere est en dehors de la board
      */
     private boolean mIsEscapeBorder;
 
@@ -49,7 +49,7 @@ public class BorderTile extends GameTile {
     /**
      * Constructeur de BorderTile
      *
-     * @param board Plateau de jeu auquel appartient le tiles de type frontiere
+     * @param board Plateau de jeu auquel appartient le tile de type frontiere
      * @param name Nom du tile
      * @param isEscapeBorder true si la frontiere represente les echapatoires
      * pour les pions dans les 4 coins du board
@@ -58,24 +58,48 @@ public class BorderTile extends GameTile {
         super(board, name, -99);
         this.mIsEscapeBorder = isEscapeBorder;
     }
-    
+
+    /**
+     * Constructeur de BorderTile
+     * 
+     * @param board Plateau de jeu auquel appartient le tile de type frontière
+     * @param stream chargement de fichier
+     * @throws IOException 
+     */
     public BorderTile(GameBoard board, DataInputStream stream) throws IOException {
         super(board, stream);
         readSerialized(stream);
     }
 
+    
+    /**
+     * ressort une BorderTile d'une sauvegarde 
+     * 
+     * @param data cible de sérialisation
+     * @throws IOException 
+     */
     @Override
     public final void readSerialized(DataInputStream data) throws IOException {
         super.readSerialized(data);
         mIsEscapeBorder = data.readBoolean();
     }
 
+    /**
+     * Serialize la tile dans le DataOutputStream indiqué
+     * 
+     * @param data La cible de serialisation
+     * @throws IOException 
+     */
     @Override
     public void serializeTo(DataOutputStream data) throws IOException {
         super.serializeTo(data);
         data.writeBoolean(mIsEscapeBorder);
     }
-    
+
+    /**
+     * Getter qui récupère le type de la tile
+     * @return type de la tile
+     */
     @Override
     public int getType() {
         return TILE_BORDER;
@@ -84,6 +108,10 @@ public class BorderTile extends GameTile {
     //--------------------------------------------------------------------------
     //GETTERS                                                                  |
     //--------------------------------------------------------------------------
+   /**
+    * Getter pour savoir si il y a une tile frontière en dehors de la board
+    * @return un boolean si oui ou non il y en a une
+    */
     public boolean isEscapeBorder() {
         return mIsEscapeBorder;
     }
